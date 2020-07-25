cb776a66a04a6e0d36766099e440c1cc00dd6d2b
Peter Cossey <peter@cossey.ocm.au>
2020-07-25 23:18:46 +1000

    First stab at displaying products.

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/crystal-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
     `gatsby-transformer-csv`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,60 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <div style={{
+      background: '#f4f4f4'
+    }}>
+      {data.allCatalogueCsv.edges.map(({ node }, index) => (
+        <article key={index} style={{
+          display: 'inline-block',
+          margin: '0.5em',
+          border: 'solid 1px #999',
+          background: 'white'
+        }}>
+          <h3>{node.name}</h3>
+          <h4>{node.category}</h4>
+          <p>{node.size}</p>
+          <p>{node.price_per_unit}</p>
+          <p><strong>Pieces per pack:</strong> {node.pieces_per_pack}</p>
+          <p><a href={`https://swarovski-professional.com/en-WORLD${node.url}`} target="_blank">More info</a></p>
+          <img src={`https://swarovski-professional.com${node.thumbnail_2x}`} alt=""/>
+        </article>
+      ))}
+    </div>
   </Layout>
 )
+
+export const query = graphql`
+  query IndexQuery {
+    allCatalogueCsv(limit: 756) {
+      edges {
+        node {
+          category
+          colour
+          mat_no
+          id
+          name
+          pieces_per_pack
+          price_per_unit
+          size
+          subtitle
+          suggested_mat_no
+          summary
+          thumbnail
+          thumbnail_2x
+          url
+          variant
+          variants_info
+          vector
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
