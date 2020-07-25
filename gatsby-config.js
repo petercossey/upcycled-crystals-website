b990353fa53413868e1d5ab784ddc8b4fc9339e3
Peter Cossey <peter@cossey.ocm.au>
2020-07-25 17:39:59 +1000

    Clean up Gatsby starter.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Upcycled Crystal Inventory`,
+    description: `Inventory browser for upcycled crystal products.`,
+    author: `@petercossey`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,15 +18,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Upcycled Crystal Inventory`,
+        short_name: `Upcycled Crystals`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FF00FF`,
+        theme_color: `#FF00FF`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/crystal-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-transformer-csv`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
